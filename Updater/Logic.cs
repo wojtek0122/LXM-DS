e54a088e7d9616fe1aafd860702bf7d599320f05
Wojciech <wojtek0122@wp.pl>
2020-04-05 22:52:28 +0200

    Add Updater App

---
+++ Updater/Logic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace Updater
+{
+    class Logic
+    {
+        public string _path;
+
+        public void ExtractZip(string Version)
+        {
+            Console.WriteLine(Version);
+        }
+
+        public void RunApp(string PathToFile)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(PathToFile);
+                Console.WriteLine("Open App - " + PathToFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Open App ERROR - " + ex.ToString());
+            }
+        }
+
+        public void ParsePathFromXML()
+        {
+            string _parsedPath = "";
+            try
+            {
+                System.Xml.XmlReader _xmlReader = System.Xml.XmlReader.Create("..\\Config.xml");
+                while (_xmlReader.Read())
+                {
+                    if ((_xmlReader.NodeType == System.Xml.XmlNodeType.Element) && (_xmlReader.Name == "CONFIG"))
+                    {
+                        if (_xmlReader.HasAttributes)
+                        {
+                            _parsedPath = String.Format(_xmlReader.GetAttribute("PATH"));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            _path = _parsedPath;
+        }
+    }
+}
---
+++ Updater/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic _logic = new Logic();
+
+            _logic.ParsePathFromXML();
+            _logic.ExtractZip(args[0]);
+            _logic.RunApp(_logic._path + @"bin\Debug\LXM-DS.exe");
+
+            Console.ReadKey();
+        }
+    }
+}
